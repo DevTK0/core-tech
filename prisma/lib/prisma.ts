ad6fb009c77da7906d432c19440e07525ddecda8
Terence Kong <tko.developer.02@gmail.com>
2022-08-11 14:58:42 +0800

    Fixed Prisma multiple instantiations.

--- prisma/lib/prisma.ts
+++ prisma/lib/prisma.ts
@@ -1,6 +1,15 @@
 import { Prisma as PrismaNamespace, PrismaClient as Client } from "@prisma/client"
 
-const PrismaClient: Client = new Client();
+declare global {
+    // allow global `var` declarations
+    // eslint-disable-next-line no-var
+    var prisma: Client | undefined
+}
+
+if (!global.prisma) {
+    global.prisma = new Client();
+}
+let PrismaClient: Client = global.prisma;
 
 export {
     PrismaClient,
