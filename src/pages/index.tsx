1c7d121d78cb0ddf0f55941c17021b0e11f5d3d8
Terence Kong <tko.developer.02@gmail.com>
2022-08-14 17:15:58 +0800

    Cleanup.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,8 @@
 import type { NextPage } from 'next'
-import { useSession, signIn, signOut } from 'next-auth/react'
 import Header  from '../components/Header';
 import SimpleTable  from '../components/SimpleTable';
-import style from '../styles/global.module.css'
-
-const handleSignin = (e: React.SyntheticEvent) => {
-  e.preventDefault()
-  signIn()
-}    
-
-const handleSignout = (e: React.SyntheticEvent) => {
-  e.preventDefault()
-  signOut()
-}
 
 const Home: NextPage = () => {
-  
-  const { data: session } = useSession();
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6">
