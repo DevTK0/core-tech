8ac6eee9cc479f3c9d59824b3c361aec2b1ae7d2
Terence Kong <tko.developer.02@gmail.com>
2022-08-17 14:35:17 +0800

    Added Battle placeholder page.

--- src/pages/battle.tsx
+++ src/pages/battle.tsx
@@ -1,17 +1,24 @@
-import type { NextPage } from 'next'
-import Header  from '../components/Header';
-import SimpleTable  from '../components/SimpleTable';
+import type { NextPage } from "next";
+import Image from "next/image";
+import Header from "../components/Header";
+import SimpleTable from "../components/SimpleTable";
 
 const Home: NextPage = () => {
-
   return (
     <div className="mx-auto px-4 sm:px-6">
-        <Header />
-        <main>
-            
-        </main>
+      <Header />
+      <main>
+        <div className="w-5/6 h-[64rem] relative m-auto">
+          <Image
+            className="object-contain object-top"
+            src="/battle_placeholder.jpeg"
+            alt=""
+            layout="fill"
+          />
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
