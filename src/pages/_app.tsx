0fc131dbcc2a2b020c5cf1c567dab5c8c5ee8ed8
Terence Kong <tko.developer.02@gmail.com>
2022-09-15 22:14:56 +0800

    Updated site title.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 />
                 <meta name="description" content="Description" />
                 <meta name="keywords" content="Keywords" />
-                <title>Next.js PWA Example</title>
+                <title>Familiar Beasts</title>
 
                 <link rel="manifest" href="/manifest.json" />
                 <link
