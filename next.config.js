4052c96bb13de6dddf98bc73c51ce5ec36f02c68
Terence Kong <tko.developer.02@gmail.com>
2022-09-16 16:52:22 +0800

    Removed warnings.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV === "production";
+
 const withPWA = require("next-pwa")({
     dest: "public",
+    disable: prod ? false : true,
 });
 
 const nextConfig = {
