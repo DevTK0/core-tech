5e84704370d655eaf655602bb1876075f9e804e9
Terence Kong <tko.developer.02@gmail.com>
2022-09-19 10:25:44 +0800

    Implemented zoom.

--- src/pages/battleui.tsx
+++ src/pages/battleui.tsx
@@ -10,14 +10,8 @@ import {
     ArrowPathIcon,
 } from "@heroicons/react/24/outline";
 
-const moveTo = (
-    canvas: RefObject<HTMLInputElement>,
-    toggle: any,
-    setToggle: any
-) => {
-    // if (toggle) canvas.current!.style.transform = "translate(100px, 0px)";
-    // else canvas.current!.style.transform = "translate(0px, 0px)";
-
+const moveTo = (zoom: any, setZoom: any, toggle: any, setToggle: any) => {
+    setZoom(!zoom);
     setToggle(!toggle);
 };
 
@@ -31,6 +25,13 @@ const Home: NextPage = () => {
     const [height, setHeight] = useState(0);
 
     const [shift, setShift] = useState(true);
+    const [zoom1, setZoom1] = useState(false);
+    const [zoom2, setZoom2] = useState(false);
+    const [zoom3, setZoom3] = useState(false);
+    const [zoom4, setZoom4] = useState(false);
+    const [zoom5, setZoom5] = useState(false);
+    const [zoom6, setZoom6] = useState(false);
+
     const [showMsgBox, setShowMsgBox] = useState(false);
 
     const [transform, setTransform] = useState({});
@@ -73,8 +74,28 @@ const Home: NextPage = () => {
                         {/* ====== Background Layer ====== */}
                         <div
                             ref={canvas}
-                            className={`${
-                                shift && "-translate-x-40"
+                            className={`${shift && "-translate-x-40"} ${
+                                zoom1 &&
+                                "-translate-x-[30vw] translate-y-[30vh] scale-150"
+                            } 
+                            ${
+                                zoom2 &&
+                                "-translate-x-[50vw] translate-y-[30vh] scale-150"
+                            } ${
+                                zoom3 &&
+                                "-translate-x-[70vw] translate-y-[30vh] scale-150"
+                            }
+                            ${
+                                zoom4 &&
+                                "translate-x-[20vw] -translate-y-[30vh] scale-150"
+                            }
+                            ${
+                                zoom5 &&
+                                "-translate-x-[0vw] -translate-y-[30vh] scale-150"
+                            }
+                            ${
+                                zoom6 &&
+                                "-translate-x-[20vw] -translate-y-[30vh] scale-150"
                             } absolute top-0 left-0 h-[100vh] max-h-[50vw] w-[200vh] max-w-[100vw] transition duration-300`}
                         >
                             {/* <div className="inline border-2 bg-cyan-700">
@@ -86,30 +107,30 @@ const Home: NextPage = () => {
                                     height={20000}
                                 />
                             </div> */}
-                            <div className="h-[100vh] w-[400vh] bg-green-400"></div>
+                            <div className="h-[200vh] w-[400vw] -translate-x-40 -translate-y-40 bg-green-400"></div>
                             <div className="absolute -right-40 top-0 flex flex-row items-end">
                                 <div
                                     ref={mon1}
                                     className="mr-14 inline-block h-[10vw] w-[10vw] rounded-[50%] bg-gray-500"
                                 >
-                                    3
+                                    1
                                 </div>
                                 <div className="mr-14 inline-block h-[10vw] w-[10vw] rounded-[50%] bg-gray-500">
                                     2
                                 </div>
                                 <div className="mr-14 inline-block h-[20vw] w-[10vw] rounded-[50%] bg-gray-500">
-                                    1
+                                    3
                                 </div>
                             </div>
                             <div className="absolute left-48 bottom-20 flex flex-row items-end">
                                 <div className="mr-14 inline-block h-[10vw] w-[10vw] rounded-[50%] bg-gray-500">
-                                    1
+                                    4
                                 </div>
                                 <div className="mr-14 inline-block h-[10vw] w-[10vw] rounded-[50%] bg-gray-500">
-                                    2
+                                    5
                                 </div>
                                 <div className="mr-14 inline-block h-[20vw] w-[10vw] rounded-[50%] bg-gray-500">
-                                    3
+                                    6
                                 </div>
                             </div>
                         </div>
@@ -165,7 +186,9 @@ const Home: NextPage = () => {
                             <button
                                 type="button"
                                 className={`mb-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
-                                onClick={() => moveTo(canvas, shift, setShift)}
+                                onClick={() =>
+                                    moveTo(zoom1, setZoom1, shift, setShift)
+                                }
                             >
                                 <ArrowPathIcon
                                     className="h-5 w-5 md:h-6 md:w-6"
@@ -178,22 +201,78 @@ const Home: NextPage = () => {
                                     className={`${
                                         showMenu ? "" : "hidden"
                                     }  mr-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
+                                    onClick={() =>
+                                        moveTo(zoom1, setZoom1, shift, setShift)
+                                    }
                                 >
-                                    <ShieldExclamationIcon
-                                        className="h-5 w-5 md:h-6 md:w-6"
-                                        aria-hidden="true"
-                                    />
+                                    <div className="h-5 w-5 md:h-6 md:w-6">
+                                        1
+                                    </div>
                                 </button>
                                 <button
                                     type="button"
                                     className={`${
                                         showMenu ? "" : "hidden"
                                     }  mr-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
+                                    onClick={() =>
+                                        moveTo(zoom2, setZoom2, shift, setShift)
+                                    }
                                 >
-                                    <BoltIcon
-                                        className="h-5 w-5 md:h-6 md:w-6"
-                                        aria-hidden="true"
-                                    />
+                                    <div className="h-5 w-5 md:h-6 md:w-6">
+                                        2
+                                    </div>
+                                </button>
+                                <button
+                                    type="button"
+                                    className={`${
+                                        showMenu ? "" : "hidden"
+                                    }  mr-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
+                                    onClick={() =>
+                                        moveTo(zoom3, setZoom3, shift, setShift)
+                                    }
+                                >
+                                    <div className="h-5 w-5 md:h-6 md:w-6">
+                                        3
+                                    </div>
+                                </button>
+                                <button
+                                    type="button"
+                                    className={`${
+                                        showMenu ? "" : "hidden"
+                                    }  mr-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
+                                    onClick={() =>
+                                        moveTo(zoom4, setZoom4, shift, setShift)
+                                    }
+                                >
+                                    <div className="h-5 w-5 md:h-6 md:w-6">
+                                        4
+                                    </div>
+                                </button>
+                                <button
+                                    type="button"
+                                    className={`${
+                                        showMenu ? "" : "hidden"
+                                    }  mr-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
+                                    onClick={() =>
+                                        moveTo(zoom5, setZoom5, shift, setShift)
+                                    }
+                                >
+                                    <div className="h-5 w-5 md:h-6 md:w-6">
+                                        5
+                                    </div>
+                                </button>
+                                <button
+                                    type="button"
+                                    className={`${
+                                        showMenu ? "" : "hidden"
+                                    }  mr-5 items-center rounded-full border border-transparent bg-white p-1.5 text-red-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 md:p-3`}
+                                    onClick={() =>
+                                        moveTo(zoom6, setZoom6, shift, setShift)
+                                    }
+                                >
+                                    <div className="h-5 w-5 md:h-6 md:w-6">
+                                        6
+                                    </div>
                                 </button>
                                 <button
                                     type="button"
